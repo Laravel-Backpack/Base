52aae7b47238787b54c9daf5a4c7bd95f5e2535f
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-03-04 17:35:21 +0200

    comments in menu view

--- src/resources/views/inc/menu.blade.php
+++ src/resources/views/inc/menu.blade.php
@@ -1,20 +1,31 @@
+<div class="navbar-custom-menu pull-left">
+    <ul class="nav navbar-nav">
+        <!-- =================================================== -->
+        <!-- ========== Top menu items (ordered left) ========== -->
+        <!-- =================================================== -->
+
+        <!-- <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> <span>Home</span></a></li> -->
+
+        <!-- ========== End of top menu left items ========== -->
+    </ul>
+</div>
+
+
 <div class="navbar-custom-menu">
     <ul class="nav navbar-nav">
-      <!-- Laravel User Account -->
-      @if (Auth::guest())
+      <!-- ========================================================= -->
+      <!-- ========== Top menu right items (ordered left) ========== -->
+      <!-- ========================================================= -->
+
+      <!-- <li><a href="{{ url('/') }}"><i class="fa fa-home"></i> <span>Home</span></a></li> -->
+
+        @if (Auth::guest())
             <li><a href="{{ url('admin/login') }}">Login</a></li>
             <li><a href="{{ url('admin/register') }}">Register</a></li>
         @else
             <li><a href="{{ url('admin/logout') }}"><i class="fa fa-btn fa-sign-out"></i> Logout</a></li>
-            <!-- <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                    {{ Auth::user()->name }} <span class="caret"></span>
-                </a>
-
-                <ul class="dropdown-menu" role="menu">
-                    <li><a href="{{ url('admin/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                </ul>
-            </li> -->
         @endif
+
+       <!-- ========== End of top menu right items ========== -->
     </ul>
 </div>
