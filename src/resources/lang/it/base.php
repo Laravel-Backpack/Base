7d35ac7c38297835b4b2020a1ec744dcd768fbd1
Federico Liva <Federico Liva>
2016-06-06 20:06:33 +0200

    Add italian translation

---
+++ src/resources/lang/it/base.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Backpack\Base Language Lines
+	|--------------------------------------------------------------------------
+	*/
+
+	'registration_closed'  => 'Le registrazioni sono chiuse.',
+	'first_page_you_see'   => 'La prima pagina che vedi dopo il login',
+	'login_status'         => 'Stato autenticazione',
+	'logged_in'            => 'Sei autenticato!',
+	'toggle_navigation'    => 'Apri/chiudi navigazione',
+	'administration'       => 'AMMINISTRAZIONE',
+	'user'                 => 'UTENTE',
+	'logout'               => 'Esci',
+	'login'                => 'Accedi',
+	'register'             => 'Registrati',
+	'name'                 => 'Nome',
+	'email_address'        => 'Indirizzo E-mail',
+	'password'             => 'Password',
+	'confirm_password'     => 'Conferma Password',
+	'remember_me'          => 'Ricordami',
+	'forgot_your_password' => 'Hai dimenticato la password?',
+	'reset_password'       => 'Reimposta password',
+	'send_reset_link'      => 'Invia link di reset',
+	'click_here_to_reset'  => 'Clicca qui per reimpostare la tua password',
+	'unauthorized'         => 'Non autorizzato.',
+	'dashboard'            => 'Dashboard'
+];
