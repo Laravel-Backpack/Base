c4a346226ef3d011fcf6f792af3b27b9d949c064
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-06-13 17:09:09 +0300

    Dashboard sidebar link stayed active all the time

--- src/resources/views/inc/sidebar.blade.php
+++ src/resources/views/inc/sidebar.blade.php
@@ -19,7 +19,7 @@
           <!-- ================================================ -->
           <!-- ==== Recommended place for admin menu items ==== -->
           <!-- ================================================ -->
-          <li><a href="{{ url('admin') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+          <li><a href="{{ url('admin/dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
 
 
           <!-- ======================================= -->
