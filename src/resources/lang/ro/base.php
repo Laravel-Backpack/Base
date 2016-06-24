1739c77fa4f8731f2aa9ff95616089567a9ac461
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-06-24 14:20:11 +0300

    Added Romanian translation.

---
+++ src/resources/lang/ro/base.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\Base Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'registration_closed'  => 'Înregistrarea este închisă.',
+    'first_page_you_see'   => 'Prima pagină pe care o vezi după logare',
+    'login_status'         => 'Starea de conectare',
+    'logged_in'            => 'Sunteți autentificat!',
+    'toggle_navigation'    => 'Arată/ascunde meniul',
+    'administration'       => 'ADMINISTRARE',
+    'user'                 => 'UTILIZATOR',
+    'logout'               => 'Ieșire din cont',
+    'login'                => 'Autentificare',
+    'register'             => 'Înregistrare cont nou',
+    'name'                 => 'Nume',
+    'email_address'        => 'Adresă de email',
+    'password'             => 'Parolă',
+    'confirm_password'     => 'Confirmare parolă',
+    'remember_me'          => 'Ține-mă minte',
+    'forgot_your_password' => 'Ți-ai uitat parola?',
+    'reset_password'       => 'Resetare parolă',
+    'send_reset_link'      => 'Trimite adresa de resetare parolă',
+    'click_here_to_reset'  => 'Apasa aici pentru a reseta parola',
+    'unauthorized'         => 'Acces neautorizat.',
+    'dashboard'            => 'Panou de administrare',
+    'handcrafted_by'       => 'Realizat de',
+    'powered_by'           => 'Powered by',
+];
