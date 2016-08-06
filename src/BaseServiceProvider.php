45c3b1f6ceeeac3af3e8e4b71868178dd07e7d22
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-08-06 10:42:14 +0300

    fix dashboard route

--- src/BaseServiceProvider.php
+++ src/BaseServiceProvider.php
@@ -68,23 +68,23 @@ class BaseServiceProvider extends ServiceProvider
 
         $router->group(['namespace' => 'Backpack\Base\app\Http\Controllers'], function ($router) {
 
-            // if not otherwise configured, setup the auth routes
-            if (config('backpack.base.setup_auth_routes')) {
-                Route::group(['middleware' => 'web', 'prefix' => config('backpack.base.route_prefix')], function () {
-                    // Admin authentication routes
-                    Route::auth();
-                });
-            }
+            Route::group(['middleware' => 'web', 'prefix' => config('backpack.base.route_prefix')], function () {
 
-            // if not otherwise configured, setup the dashboard routes
-            if (config('backpack.base.setup_dashboard_routes')) {
-                // Admin dashboard routes
-                Route::get('dashboard', 'AdminController@dashboard');
-                Route::get('/', function () {
-                    // The '/admin' route is not to be used as a page, because it breaks the menu's active state.
-                    return redirect(config('backpack.base.route_prefix').'/dashboard');
-                });
-            }
+                // if not otherwise configured, setup the auth routes
+                if (config('backpack.base.setup_auth_routes')) {
+                        Route::auth();
+                }
+
+                // if not otherwise configured, setup the dashboard routes
+                if (config('backpack.base.setup_dashboard_routes')) {
+                    Route::get('dashboard', 'AdminController@dashboard');
+                    Route::get('/', function () {
+                        // The '/admin' route is not to be used as a page, because it breaks the menu's active state.
+                        return redirect(config('backpack.base.route_prefix').'/dashboard');
+                    });
+                }
+
+            });
         });
     }
 
