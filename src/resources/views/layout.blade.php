721087dcded3971ba356cfa93ae2ae981b07729e
Zachary Schuessler <zlschuessler@gmail.com>
2016-11-30 13:34:48 -0700

    update font awesome version in layout.php (4.40->4.70)
    Font awesome version 4.4.0 to 4.7.0 (good number of missing icons between these versions)

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -17,7 +17,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/AdminLTE.min.css">
