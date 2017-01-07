9687f5c17cc3f0173fa0d590943d2a26701f4bbc
Stan Daniëls <stan@inovamedia.nl>
2017-01-07 21:24:21 +0100

    Add Dutch translations
    Signed-off-by: Stan Daniëls <stan@inovamedia.nl>

---
+++ src/resources/lang/nl/base.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\Base Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'registration_closed'  => 'Registratie is gesloten.',
+    'first_page_you_see'   => 'De eerste pagina die je ziet na inloggen',
+    'login_status'         => 'Loginstatus',
+    'logged_in'            => 'Je bent ingelogd!',
+    'toggle_navigation'    => 'Schakel menu in/uit',
+    'administration'       => 'ADMINISTRATIE',
+    'user'                 => 'GEBRUIKER',
+    'logout'               => 'Uitloggen',
+    'login'                => 'Inloggen',
+    'register'             => 'Registreer',
+    'name'                 => 'Naam',
+    'email_address'        => 'E-mailadres',
+    'password'             => 'Wachtwoord',
+    'confirm_password'     => 'Bevestig wachtwoord',
+    'remember_me'          => 'Blijf ingelogd',
+    'forgot_your_password' => 'Wachtwoord vergeten?',
+    'reset_password'       => 'Wachtwoord herstellen',
+    'send_reset_link'      => 'Stuur wachtwoord herstellen-link',
+    'click_here_to_reset'  => 'Klik hier om je wachtwoord te herstellen',
+    'unauthorized'         => 'Ongeautoriseerd.',
+    'dashboard'            => 'Dashboard',
+    'handcrafted_by'       => 'Handgemaakt door',
+    'powered_by'           => 'Mede mogelijk gemaakt door',
+];
