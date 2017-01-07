7db05658451b179075a6eab20dbc10ef0c37fb66
Sergey <mr.kregi@yandex.ru>
2017-01-07 16:10:26 +0300

    Update sidebar.blade.php
    fix mixed content and fix get first character of UTF-8 string from user name

--- src/resources/views/inc/sidebar.blade.php
+++ src/resources/views/inc/sidebar.blade.php
@@ -6,7 +6,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
           <div class="pull-left image">
-            <img src="http://placehold.it/160x160/00a65a/ffffff/&text={{ Auth::user()->name[0] }}" class="img-circle" alt="User Image">
+            <img src="https://placehold.it/160x160/00a65a/ffffff/&text={{ mb_substr(Auth::user()->name, 0, 1) }}" class="img-circle" alt="User Image">
           </div>
           <div class="pull-left info">
             <p>{{ Auth::user()->name }}</p>
