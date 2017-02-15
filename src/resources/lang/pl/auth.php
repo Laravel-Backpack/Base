c2ca22972089fa4d5bc3c453b651e619012f1f18
Grzegorz Miskiewicz <djmassive@o2.pl>
2017-02-15 01:20:17 +0100

    pl/auth.php
    Polish translation

---
+++ src/resources/lang/pl/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    | @author Grzegorz Miskiewicz
+    | @www    www.avatec.pl
+    |
+    */
+
+    'failed'   => 'Podane dane logowania nie są poprawne.',
+    'throttle' => 'Za dużo prób logowania. Prosimy spróbować ponownie za :seconds sekund.',
+
+];
