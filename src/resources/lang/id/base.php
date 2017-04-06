4e1d48c03831f951485aadd9d6f68dd0b134928e
Nakamura Agatha <nakamura.agatha@gmail.com>
2017-04-06 13:57:39 +0700

    add indonesia translation

---
+++ src/resources/lang/id/base.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\Base Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'registration_closed'  => 'Pendaftaran di tutup.',
+    'first_page_you_see'   => 'Halaman pertama yang Anda lihat setelah login',
+    'login_status'         => 'Status Masuk',
+    'logged_in'            => 'Anda sudah login!',
+    'toggle_navigation'    => 'Navigasi beralih',
+    'administration'       => 'Administrasi',
+    'user'                 => 'Pengguna',
+    'logout'               => 'Keluar',
+    'login'                => 'Masuk',
+    'register'             => 'Daftar',
+    'name'                 => 'Nama',
+    'email_address'        => 'Alamat Email',
+    'password'             => 'Sandi',
+    'confirm_password'     => 'Konfirmasi Sandi',
+    'remember_me'          => 'Ingat Saya',
+    'forgot_your_password' => 'Lupa kata sandi?',
+    'reset_password'       => 'Setel ulang sandi',
+    'send_reset_link'      => 'Kirim link reset kata sandi',
+    'click_here_to_reset'  => 'Klik Disini untuk menyetel ulang kata sandi',
+    'unauthorized'         => 'Tidak Sah.',
+    'dashboard'            => 'Dashboard',
+    'handcrafted_by'       => 'Handcrafted by',
+    'powered_by'           => 'Powered by',
+];
