86a7ca7b96ce5754f77ed82cc79b7dc3e7fd1066
Alashov Berkeli <yunus.alashow@gmail.com>
2017-04-06 17:40:27 -0500

    replaced html comments with blade comments

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -15,45 +15,45 @@
     @yield('before_styles')
 
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.5 -->
+    {{-- Bootstrap 3.3.5 --}}
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
+    {{-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. --}}
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/dist/css/skins/_all-skins.min.css">
 
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/') }}/plugins/pace/pace.min.css">
     <link rel="stylesheet" href="{{ asset('vendor/backpack/pnotify/pnotify.custom.min.css') }}">
 
-    <!-- BackPack Base CSS -->
+    {{-- BackPack Base CSS --}}
     <link rel="stylesheet" href="{{ asset('vendor/backpack/backpack.base.css') }}">
 
     @yield('after_styles')
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    {{-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --}}
+    {{-- WARNING: Respond.js doesn't work if you view the page via file:// --}}
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
 <body class="hold-transition {{ config('backpack.base.skin') }} sidebar-mini">
-    <!-- Site wrapper -->
+    {{-- Site wrapper --}}
     <div class="wrapper">
 
       <header class="main-header">
-        <!-- Logo -->
+        {{-- Logo --}}
         <a href="{{ url('') }}" class="logo">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
+          {{-- mini logo for sidebar mini 50x50 pixels --}}
           <span class="logo-mini">{!! config('backpack.base.logo_mini') !!}</span>
-          <!-- logo for regular state and mobile devices -->
+          {{-- logo for regular state and mobile devices --}}
           <span class="logo-lg">{!! config('backpack.base.logo_lg') !!}</span>
         </a>
-        <!-- Header Navbar: style can be found in header.less -->
+        {{-- Header Navbar: style can be found in header.less --}}
         <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle button-->
+          {{-- Sidebar toggle button--}}
           <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">{{ trans('backpack::base.toggle_navigation') }}</span>
             <span class="icon-bar"></span>
@@ -65,26 +65,26 @@
         </nav>
       </header>
 
-      <!-- =============================================== -->
+      {{-- =============================================== --}}
 
       @include('backpack::inc.sidebar')
 
-      <!-- =============================================== -->
+      {{-- =============================================== --}}
 
-      <!-- Content Wrapper. Contains page content -->
+      {{-- Content Wrapper. Contains page content --}}
       <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
+        {{-- Content Header (Page header) --}}
          @yield('header')
 
-        <!-- Main content -->
+        {{-- Main content --}}
         <section class="content">
 
           @yield('content')
 
         </section>
-        <!-- /.content -->
+        {{-- /.content --}}
       </div>
-      <!-- /.content-wrapper -->
+      {{-- /.content-wrapper --}}
 
       <footer class="main-footer">
         @if (config('backpack.base.show_powered_by'))
@@ -95,22 +95,22 @@
         {{ trans('backpack::base.handcrafted_by') }} <a target="_blank" href="{{ config('backpack.base.developer_link') }}">{{ config('backpack.base.developer_name') }}</a>.
       </footer>
     </div>
-    <!-- ./wrapper -->
+    {{-- ./wrapper --}}
 
 
     @yield('before_scripts')
 
-    <!-- jQuery 2.2.0 -->
+    {{-- jQuery 2.2.0 --}}
     <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
     <script>window.jQuery || document.write('<script src="{{ asset('vendor/adminlte') }}/plugins/jQuery/jQuery-2.2.0.min.js"><\/script>')</script>
-    <!-- Bootstrap 3.3.5 -->
+    {{-- Bootstrap 3.3.5 --}}
     <script src="{{ asset('vendor/adminlte') }}/bootstrap/js/bootstrap.min.js"></script>
     <script src="{{ asset('vendor/adminlte') }}/plugins/pace/pace.min.js"></script>
     <script src="{{ asset('vendor/adminlte') }}/plugins/slimScroll/jquery.slimscroll.min.js"></script>
     <script src="{{ asset('vendor/adminlte') }}/plugins/fastclick/fastclick.js"></script>
     <script src="{{ asset('vendor/adminlte') }}/dist/js/app.min.js"></script>
 
-    <!-- page script -->
+    {{-- page script --}}
     <script type="text/javascript">
         // To make Pace works on Ajax calls
         $(document).ajaxStart(function() { Pace.restart(); });
@@ -136,7 +136,7 @@
 
     @yield('after_scripts')
 
-    <!-- JavaScripts -->
+    {{-- JavaScripts --}}
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
