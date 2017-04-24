fcba9239757156d9f56c95d84d52731b9c6acd1e
Ropez <ropez.erik@gmail.com>
2017-04-24 10:44:58 +0300

    Latvian translation.

---
+++ src/resources/lang/lv/base.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Backpack\Base Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'registration_closed'  => 'Reģistrācija aizvērta.',
+    'first_page_you_see'   => 'Pirmā lapa ko redzat pēc pieslēgšanās',
+    'login_status'         => 'Pieslēgšanās statuss',
+    'logged_in'            => 'Jūs esat pieslēgušies!',
+    'toggle_navigation'    => 'Pārslēgt navigāciju',
+    'administration'       => 'ADMINISTRĀCIJA',
+    'user'                 => 'LIETOTĀJS',
+    'logout'               => 'Iziet',
+    'login'                => 'Pieslēgties',
+    'register'             => 'Registrēties',
+    'name'                 => 'Vārds',
+    'email_address'        => 'E-Mail Adrese',
+    'password'             => 'Parole',
+    'confirm_password'     => 'Apstiprināt Paroli',
+    'remember_me'          => 'Atcerēties Mani',
+    'forgot_your_password' => 'Aizmirsāt Jūsu Paroli?',
+    'reset_password'       => 'Atjaunot Paroli',
+    'send_reset_link'      => 'Sūtīt Paroles Atjaunošanas Saiti',
+    'click_here_to_reset'  => 'Nospiediet šēit lai atjaunotu Jūsu paroli',
+    'unauthorized'         => 'Neautorizēts.',
+    'dashboard'            => 'Instrumentu panelis',
+    'handcrafted_by'       => 'Handcrafted by',
+    'powered_by'           => 'Powered by',
+];
