12e23d3b80245e0f57ae0aad9ccdf85e35d079ef
Jordan Kniest <mail@jkniest.de>
2017-09-02 14:30:17 +0200

    Fixed small typo (on -> or)

--- src/config/backpack/base.php
+++ src/config/backpack/base.php
@@ -82,7 +82,7 @@ return [
     |
     | If you, your employer or your client make money by using Backpack, you need
     | to purchase a license code. A license code will be provided after purchse,
-    | which you can put here on in your ENV file.
+    | which you can put here or in your ENV file.
     |
     | More info and payment form on:
     | https://www.backpackforlaravel.com
