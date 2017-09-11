4f1b2786d102bbd2b60fdd24c27cabdb9ae72191
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-09-11 16:49:37 +0300

    removed Auth class from avatar helper

--- src/helpers.php
+++ src/helpers.php
@@ -29,11 +29,11 @@ if (!function_exists('backpack_avatar')) {
     {
         switch (config('backpack.base.avatar_type')) {
             case 'gravatar':
-                return Gravatar::fallback('https://placehold.it/160x160/00a65a/ffffff/&text='.Auth::user()->name[0])->get(Auth::user()->email);
+                return Gravatar::fallback('https://placehold.it/160x160/00a65a/ffffff/&text='.Auth::user()->name[0])->get($user->email);
                 break;
 
             case 'placehold':
-                return 'https://placehold.it/160x160/00a65a/ffffff/&text='.Auth::user()->name[0];
+                return 'https://placehold.it/160x160/00a65a/ffffff/&text='.$user->name[0];
                 break;
 
             default:
