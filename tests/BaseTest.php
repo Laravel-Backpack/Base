c4bb875de7315c7ab0f25ee1c73101963e16287d
Cristian Tabacitu <cristitabacitu@gmail.com>
2017-09-11 11:01:56 +0300

    dummy unit test

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-class BaseTest extends PHPUnit_Framework_TestCase
+class BaseTest extends BaseTestCase
 {
     /**
      * A basic test example.
