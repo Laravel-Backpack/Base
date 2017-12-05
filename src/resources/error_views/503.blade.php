b4f2ef96e5ba9bd63260db8371daa20425b4f32d
Mariusz Tarnaski <mariusz@greenparrot.pl>
2017-12-05 12:33:57 +0100

    Correct title for 503 error page

--- src/resources/error_views/503.blade.php
+++ src/resources/error_views/503.blade.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>{{ config('backpack.base.project_name') }} Error 500</title>
+    <title>{{ config('backpack.base.project_name') }} Error 503</title>
 
     <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
