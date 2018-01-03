72347b39cc0fe2be290e3d7764716d569d7b0463
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-01-03 09:37:04 +0200

    sidebar menu added links to user image and user name

--- src/resources/views/inc/sidebar.blade.php
+++ src/resources/views/inc/sidebar.blade.php
@@ -4,15 +4,8 @@
       <!-- sidebar: style can be found in sidebar.less -->
       <section class="sidebar">
         <!-- Sidebar user panel -->
-        <div class="user-panel">
-          <div class="pull-left image">
-            <img src="{{ backpack_avatar_url(Auth::user()) }}" class="img-circle" alt="User Image">
-          </div>
-          <div class="pull-left info">
-            <p>{{ Auth::user()->name }}</p>
-            <small><small><a href="{{ route('backpack.account.info') }}"><span><i class="fa fa-user-circle-o"></i> {{ trans('backpack::base.my_account') }}</span></a> &nbsp;  &nbsp; <a href="{{ backpack_url('logout') }}"><i class="fa fa-sign-out"></i> <span>{{ trans('backpack::base.logout') }}</span></a></small></small>
-          </div>
-        </div>
+        @include('backpack::inc.sidebar_user_panel')
+
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
           {{-- <li class="header">{{ trans('backpack::base.administration') }}</li> --}}
