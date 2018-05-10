cc0d0068ae35cff42ff6b9bbbd3b6cc090bcbd86
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-05-10 05:50:39 +0000

    Apply fixes from StyleCI

--- src/helpers.php
+++ src/helpers.php
@@ -132,9 +132,10 @@ if (!function_exists('mb_ucfirst')) {
      * Capitalize the first letter of a string,
      * even if that string is multi-byte (non-latin alphabet).
      *
-     * @param  string $string   String to have its first letter capitalized.
-     * @param  encoding $encoding Character encoding
-     * @return string           String with first letter capitalized.
+     * @param string   $string   String to have its first letter capitalized.
+     * @param encoding $encoding Character encoding
+     *
+     * @return string String with first letter capitalized.
      */
     function mb_ucfirst($string, $encoding = false)
     {
@@ -144,6 +145,6 @@ if (!function_exists('mb_ucfirst')) {
         $firstChar = mb_substr($string, 0, 1, $encoding);
         $then = mb_substr($string, 1, $strlen - 1, $encoding);
 
-        return mb_strtoupper($firstChar, $encoding) . $then;
+        return mb_strtoupper($firstChar, $encoding).$then;
     }
 }
