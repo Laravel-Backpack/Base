2652ca03e2929d8484232593c910f36f4bf91d54
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-11-12 13:48:01 +0200

    replaced boxes with panels

--- src/resources/views/auth/account/sidemenu.blade.php
+++ src/resources/views/auth/account/sidemenu.blade.php
@@ -1,10 +1,8 @@
-<div class="box">
+<div class="panel">
     <div class="box-body box-profile">
 	    <img class="profile-user-img img-responsive img-circle" src="{{ backpack_avatar_url(backpack_auth()->user()) }}">
 	    <h3 class="profile-username text-center">{{ backpack_auth()->user()->name }}</h3>
 	</div>
-
-	<hr class="m-t-0 m-b-0">
 
 	<ul class="nav nav-pills nav-stacked">
 
