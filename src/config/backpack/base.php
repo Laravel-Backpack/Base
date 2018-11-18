bf8f15fc7ea25822d07ef64068864bce12201899
Thomas Swonke <thomas.swonke@webfactor.de>
2018-11-19 00:37:25 +0100

    add config to fix layout

--- src/config/backpack/base.php
+++ src/config/backpack/base.php
@@ -31,6 +31,10 @@ return [
     'skin' => 'skin-purple',
     // Options: skin-black, skin-blue, skin-purple, skin-red, skin-yellow, skin-green, skin-blue-light, skin-black-light, skin-purple-light, skin-green-light, skin-red-light, skin-yellow-light
 
+    // Sidebar/Navbar layout config
+    // Options: fixed
+    'layout' => '',
+
     // Date & Datetime Format Syntax: https://github.com/jenssegers/date#usage
     // (same as Carbon)
     'default_date_format'     => 'j F Y',
--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -44,7 +44,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body class="hold-transition {{ config('backpack.base.skin') }} sidebar-mini">
+<body class="hold-transition {{ config('backpack.base.skin') }} sidebar-mini {{ config('backpack.base.layout', '') }}">
 	<script type="text/javascript">
 		/* Recover sidebar state */
 		(function () {
