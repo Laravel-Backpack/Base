e44017b2f21886ea856a7352534ec16cb94f5214
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-11-29 08:14:23 +0200

    added middleware that allows developer to use @can statements

---
+++ src/app/Http/Middleware/UseBackpackAuthGuardInsteadOfDefaultAuthGuard.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Backpack\Base\app\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class UseBackpackAuthGuardInsteadOfDefaultAuthGuard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     * @param string|null              $guard
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        app('auth')->setDefaultDriver(config('backpack.base.guard'));
+
+        return $next($request);
+    }
+}
--- src/config/backpack/base.php
+++ src/config/backpack/base.php
@@ -95,6 +95,7 @@ return [
     'middleware_class' => [
         App\Http\Middleware\CheckIfAdmin::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // \Backpack\Base\app\Http\Middleware\UseBackpackAuthGuardInsteadOfDefaultAuthGuard::class,
     ],
 
     // Alias for that middleware
