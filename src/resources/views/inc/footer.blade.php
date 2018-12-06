5234f68d278b1ae9378b2f987110f2f8da9def3d
John Puddephatt <john@jdp.org.uk>
2018-12-06 12:00:45 +0000

    Include developer link only if set in config

--- src/resources/views/inc/footer.blade.php
+++ src/resources/views/inc/footer.blade.php
@@ -3,4 +3,6 @@
       {{ trans('backpack::base.powered_by') }} <a target="_blank" href="http://backpackforlaravel.com?ref=panel_footer_link">Backpack for Laravel</a>
     </div>
 @endif
-{{ trans('backpack::base.handcrafted_by') }} <a target="_blank" href="{{ config('backpack.base.developer_link') }}">{{ config('backpack.base.developer_name') }}</a>.
+@if (config('backpack.base.developer_link') && config('backpack.base.developer_name'))
+    {{ trans('backpack::base.handcrafted_by') }} <a target="_blank" href="{{ config('backpack.base.developer_link') }}">{{ config('backpack.base.developer_name') }}</a>.
+@endif
