64648b37b6f39900f501635ae435ba0c8c3dff15
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-06-19 11:40:26 +0300

    coreui added breadcrumbs and fixed dropdown

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -1,5 +1,19 @@
 @extends('backpack::layout')
 
+@section('header')
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ url(config('backpack.base.route_prefix'), 'dashboard') }}">{{ trans('backpack::crud.admin') }}</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{{ trans('backpack::base.dashboard') }}</li>
+      </ol>
+    </nav>
+    <section class="container-fluid">
+      <h1>
+        <span class="text-capitalize">{{ trans('backpack::base.dashboard') }}</small>
+      </h1>
+    </section>
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-md-12">
--- src/resources/views/inc/menu_user_dropdown.blade.php
+++ src/resources/views/inc/menu_user_dropdown.blade.php
@@ -2,12 +2,8 @@
   <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
     <img class="img-avatar" src="{{ backpack_avatar_url(backpack_auth()->user()) }}" alt="{{ backpack_auth()->user()->name }}">
   </a>
-  <div class="dropdown-menu dropdown-menu-right">
-    <div class="dropdown-header">
-    <a class="dropdown-item" href="{{ route('backpack.account.info') }}">
-      <i class="fa fa-user"></i> {{ trans('backpack::base.my_account') }}</a>
+  <div class="dropdown-menu dropdown-menu-right mr-4">
+    <a class="dropdown-item" href="{{ route('backpack.account.info') }}"><i class="fa fa-user"></i> {{ trans('backpack::base.my_account') }}</a>
     <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="{{ backpack_url('logout') }}">
-      <i class="fa fa-lock"></i> {{ trans('backpack::base.logout') }}</a>
-  </div>
+    <a class="dropdown-item" href="{{ backpack_url('logout') }}"><i class="fa fa-lock"></i> {{ trans('backpack::base.logout') }}</a>
 </li>
