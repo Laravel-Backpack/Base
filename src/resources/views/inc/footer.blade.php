48e0eb37167ed4ddff802b1eb60d602be68158f3
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-06-25 07:44:41 +0300

    coreui centered footer

--- src/resources/views/inc/footer.blade.php
+++ src/resources/views/inc/footer.blade.php
@@ -1,10 +1,10 @@
-@if (config('backpack.base.developer_link') && config('backpack.base.developer_name'))
-	<div class="text-muted">
-    {{ trans('backpack::base.handcrafted_by') }} <a target="_blank" href="{{ config('backpack.base.developer_link') }}">{{ config('backpack.base.developer_name') }}</a>.
-    </div>
-@endif
-@if (config('backpack.base.show_powered_by'))
-    <div class="text-muted ml-auto">
-      {{ trans('backpack::base.powered_by') }} <a target="_blank" href="http://backpackforlaravel.com?ref=panel_footer_link">Backpack for Laravel</a>
+@if (config('backpack.base.show_powered_by') || config('backpack.base.developer_link'))
+    <div class="text-muted ml-auto mr-auto">
+      @if (config('backpack.base.developer_link') && config('backpack.base.developer_name'))
+      {{ trans('backpack::base.handcrafted_by') }} <a target="_blank" href="{{ config('backpack.base.developer_link') }}">{{ config('backpack.base.developer_name') }}</a>.
+      @endif
+      @if (config('backpack.base.show_powered_by'))
+      {{ trans('backpack::base.powered_by') }} <a target="_blank" href="http://backpackforlaravel.com?ref=panel_footer_link">Backpack for Laravel</a>.
+      @endif
     </div>
 @endif
