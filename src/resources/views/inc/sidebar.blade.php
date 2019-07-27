b77646fa5c73be2df6d6e8d2d16992d6736d6eef
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-07-27 11:07:50 +0300

    small sidebar comment improvement

--- src/resources/views/inc/sidebar.blade.php
+++ src/resources/views/inc/sidebar.blade.php
@@ -5,7 +5,7 @@
       <nav class="sidebar-nav overflow-hidden">
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="nav">
-          {{-- <li class="header">{{ trans('backpack::base.administration') }}</li> --}}
+          <!-- <li class="nav-title">{{ trans('backpack::base.administration') }}</li> -->
           <!-- ================================================ -->
           <!-- ==== Recommended place for admin menu items ==== -->
           <!-- ================================================ -->
