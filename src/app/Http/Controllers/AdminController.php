ab16fe99e0c65ee702749b64ec0fe6a87200f9c5
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-07-30 15:36:53 +0000

    Apply fixes from StyleCI

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -24,33 +24,33 @@ class AdminController extends Controller
         $this->data['title'] = trans('backpack::base.dashboard'); // set the page title
         $this->data['cards']['before_content'] = [
             [
-                'type' => 'simple',
+                'type'         => 'simple',
                 'wrapperClass' => 'col-sm-6 col-md-4',
-                'content' => [
+                'content'      => [
                     'header' => 'Some card title',
-                    'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
-                ]
+                    'body'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit. <i>Nulla posuere</i>, sem et porttitor mollis, massa nibh sagittis nibh, id porttitor nibh turpis sed arcu.',
+                ],
             ],
             [
-                'type' => 'simple',
+                'type'         => 'simple',
                 'wrapperClass' => 'col-sm-6 col-md-4',
-                'content' => [
+                'content'      => [
                     'header' => 'Yet another card title',
-                    'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit.',
-                ]
+                    'body'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit.',
+                ],
             ],
         ];
         $this->data['cards']['after_content'] = [
             [
-                'type' => 'simple',
+                'type'         => 'simple',
                 'wrapperClass' => 'col-sm-6 col-md-4',
-                'class' => 'card text-white bg-primary text-center',
-                'content' => [
+                'class'        => 'card text-white bg-primary text-center',
+                'content'      => [
                     // 'header' => 'Another card title',
                     'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non mi nec orci euismod venenatis. Integer quis sapien et diam facilisis facilisis ultricies quis justo. Phasellus sem <b>turpis</b>, ornare quis aliquet ut, volutpat et lectus. Aliquam a egestas elit.',
-                ]
+                ],
             ],
-            
+
         ];
 
         return view('backpack::dashboard', $this->data);
