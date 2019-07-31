5ac4359adb3cce3911e700c50c6128c3d36a50e5
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-07-31 11:03:26 +0300

    using mix helper with second argument for manifest file

--- src/resources/views/inc/scripts.blade.php
+++ src/resources/views/inc/scripts.blade.php
@@ -5,8 +5,8 @@
 @endif
 
 @if (config('backpack.base.mix_scripts') && count(config('backpack.base.mix_scripts')))
-    @foreach (config('backpack.base.mix_scripts') as $path)
-    <script type="text/javascript" src="{{ mix($path) }}"></script>
+    @foreach (config('backpack.base.mix_scripts') as $path => $manifest)
+    <script type="text/javascript" src="{{ mix($path, $manifest) }}"></script>
     @endforeach
 @endif
 
