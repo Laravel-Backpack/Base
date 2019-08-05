833f7733235ebbec763d5a262b148a3b0fb6b985
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-08-05 12:47:32 +0300

    added progress and progress_white widgets

---
+++ src/resources/views/widgets/progress_white.blade.php
@@ -0,0 +1,29 @@
+<div class="{{ $widget['wrapperClass'] ?? 'col-sm-6 col-lg-3' }}">
+  <div class="{{ $widget['class'] ?? 'card' }}">
+    <div class="card-body">
+      @if (isset($widget['value']))
+      <div class="text-value">{!! $widget['value'] !!}</div>
+      @endif
+
+      @if (isset($widget['description']))
+      <div>{!! $widget['description'] !!}</div>
+      @endif
+      
+      @if (isset($widget['progress']))
+      <div class="progress progress-xs my-2">
+        <div class="{{ $widget['progressClass'] ?? 'progress-bar bg-info' }}" role="progressbar" style="width: {{ $widget['progress']  }}%" aria-valuenow="{{ $widget['progress']  }}" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      @endif
+      
+      @if (isset($widget['hint']))
+      <small class="text-muted">{!! $widget['hint'] !!}</small>
+      @endif
+    </div>
+
+    @if (isset($widget['footer_link']))
+    <div class="card-footer px-3 py-2">
+      <a class="btn-block text-muted d-flex justify-content-between align-items-center" href="{{ $widget['footer_link'] ?? '#' }}"><span class="small font-weight-bold">{{ $widget['footer_text'] ?? 'View more' }}</span><i class="fa fa-angle-right"></i></a>
+    </div>
+    @endif
+  </div>
+</div>
