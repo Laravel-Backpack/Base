56d7c0430dc5a0da69dfa404008b13a337f91c8b
Daryl Liu <lj3lj3@gmail.com>
2019-12-30 09:36:27 +0800

    Update comment style

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -65,7 +65,7 @@ class LoginController extends Controller
     /**
      * The user has logged out of the application.
      *
-     * @param  \Illuminate\Http\Request $request
+     * @param \Illuminate\Http\Request $request
      * @return mixed
      */
     protected function loggedOut(Request $request)
